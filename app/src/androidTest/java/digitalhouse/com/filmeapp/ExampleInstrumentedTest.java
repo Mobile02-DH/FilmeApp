db410b708d04672e0a46362b83fb11ec00be71e7
Bruno <brunopinheirodigital@gmail.com>
2018-11-14 22:40:55 -0200

    Ajuste de login

--- app/src/androidTest/java/digitalhouse/com/filmeapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/digitalhouse/com/filmeapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("digitalhouse.com.filmeapp", appContext.getPackageName());
+        assertEquals("digitalhouse.com.whatchapp", appContext.getPackageName());
     }
 }
