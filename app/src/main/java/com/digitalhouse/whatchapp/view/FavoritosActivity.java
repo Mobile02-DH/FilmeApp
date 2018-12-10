8c77ed07b284f81bcb2f9dc370cb81c0ecdc88c6
Bruno Pinheiro <cena1view@gmail.com>
2018-12-10 02:12:30 -0200

    Ajustado icone logaut e favoritos

--- app/src/main/java/com/digitalhouse/whatchapp/view/FavoritosActivity.java
+++ app/src/main/java/com/digitalhouse/whatchapp/view/FavoritosActivity.java
@@ -2,41 +2,111 @@ package com.digitalhouse.whatchapp.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.digitalhouse.whatchapp.R;
+import com.digitalhouse.whatchapp.model.Usuario;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Comment;
 
 public class FavoritosActivity extends AppCompatActivity {
+
+    private static final String TAG = "PostDetailActivity";
+
+    public static final String EXTRA_POST_KEY = "post_key";
+
+    private DatabaseReference mPostReference;
+    private DatabaseReference mCommentsReference;
+    private ValueEventListener mPostListener;
+    private String mPostKey;
+    //private CommentAdapter mAdapter;
+
+    private TextView mAuthorView;
+    private TextView mTitleView;
+    private TextView mBodyView;
+    private TextView mCommentField;
+    private Button mCommentButton;
+    private RecyclerView mCommentsRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoritos);
+    }
+        // Add value event listener to the post
+        // [START post_value_event_listener]
 
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        //mDatabase.child(mAuth.getUid())(movie);
+
+
+        private void postComment () {
+            final String uid = mAuth.getUid();
+            FirebaseDatabase.getInstance().getReference().child(uid)
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // Get user information
+                            Usuario user = dataSnapshot.getValue(Usuario.class);
+                            String authorName = user.getNome();
+
+                            // Create new comment object
+                            mCommentField = findViewById(R.id.nomeUser);
+                            mCommentField.setText(authorName);
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+        }
 
     }
 
-    /*@Override
-    public void basicQueryValueListener() {
-        String myUserId = getUid();
-        Query myTopPostsQuery = databaseReference.child("user-posts").child(myUserId)
-                .orderByChild("starCount");
 
-        // [START basic_query_value_listener]
-        // My top posts by number of stars
-        myTopPostsQuery.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-                    // TODO: handle the post
-                }
-            }
+    /*private void postComment() {
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // Getting Post failed, log a message
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-                // ...
-            }
-        });
-        // [END basic_query_value_listener]*/
-    }
+        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        final String uid = mAuth.getUid();
+        FirebaseDatabase.getInstance().getReference().child(uid)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user information
+                        User user = dataSnapshot.getValue(User.class);
+                        String authorName = user.username;
+
+                        // Create new comment object
+                        String commentText = mCommentField.getText().toString();
+                        Comment comment = new Comment(uid, authorName, commentText);
+
+                        // Push the comment, it will appear in the list
+                        mCommentsReference.push().setValue(comment);
+
+                        // Clear the field
+                        mCommentField.setText(null);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });*/
+
