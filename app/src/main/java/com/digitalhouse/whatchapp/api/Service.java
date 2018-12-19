93860cb872de9bf044317134d80a065f5f1c059a
FelipeToniolo <ftoniolo.aat@gmail.com>
2018-12-18 23:30:44 -0200

    iniciando trailers

--- app/src/main/java/com/digitalhouse/whatchapp/api/Service.java
+++ app/src/main/java/com/digitalhouse/whatchapp/api/Service.java
@@ -2,6 +2,7 @@ package com.digitalhouse.whatchapp.api;
 
 import com.digitalhouse.whatchapp.model.MoviesResponse;
 import com.digitalhouse.whatchapp.model.SeriesResponse;
+import com.digitalhouse.whatchapp.model.TrailerResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -23,5 +24,11 @@ public interface Service {
             @Query("api_key") String apiKey,
             @Query("language") String language,
             @Query("page") int page);
+
+    @GET("movie/{movie_id}/videos")
+    Call<TrailerResponse> getMovieTrailer(
+            @Path("movie_id")int id,
+            @Query("language") String language,
+            @Query("api_key") String apiKey);
 
 }
--- app/src/main/java/com/digitalhouse/whatchapp/view/DetailActivity.java
+++ app/src/main/java/com/digitalhouse/whatchapp/view/DetailActivity.java
@@ -5,20 +5,44 @@ import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.digitalhouse.whatchapp.BuildConfig;
 import com.digitalhouse.whatchapp.R;
+import com.digitalhouse.whatchapp.adapter.TrailerAdapter;
+import com.digitalhouse.whatchapp.api.Client;
+import com.digitalhouse.whatchapp.api.Service;
+import com.digitalhouse.whatchapp.model.Trailer;
+import com.digitalhouse.whatchapp.model.TrailerResponse;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class DetailActivity extends AppCompatActivity {
+
     TextView nameOfMovie, plotSynopsis, userRating, releaseDate;
     ImageView imageView;
+    private RecyclerView recyclerView;
+    private TrailerAdapter adapter;
+    private List<Trailer> trailerList;
+    private List<Trailer> trailer = new ArrayList<>();
+    ;
+    int movie_id =1;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -35,7 +59,7 @@ public class DetailActivity extends AppCompatActivity {
         releaseDate = findViewById(R.id.releasedate);
 
         Intent intentThatStartedThisActivity = getIntent();
-        if (intentThatStartedThisActivity.hasExtra("original_title")){
+        if (intentThatStartedThisActivity.hasExtra("original_title")) {
 
             String thumbnail = getIntent().getExtras().getString("poster_path");
             String movieName = getIntent().getExtras().getString("original_title");
@@ -56,9 +80,10 @@ public class DetailActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "No API Data", Toast.LENGTH_SHORT).show();
         }
+        initViews();
     }
 
-    private  void initCollapsingToolbar(){
+    private void initCollapsingToolbar() {
         final CollapsingToolbarLayout collapsingToolbarLayout =
                 findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle(" ");
@@ -73,17 +98,63 @@ public class DetailActivity extends AppCompatActivity {
 
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                if (scrollRange == 0){
+                if (scrollRange == 0) {
                     scrollRange = appBarLayout.getTotalScrollRange();
                 }
-                if (scrollRange + verticalOffset == 0){
+                if (scrollRange + verticalOffset == 0) {
                     collapsingToolbarLayout.setTitle(getString(R.string.movie_details));
                     isShow = true;
-                }else if (isShow){
+                } else if (isShow) {
                     collapsingToolbarLayout.setTitle(" ");
                     isShow = false;
                 }
             }
         });
+    }
+
+    private void initViews() {
+        trailerList = new ArrayList<>();
+        adapter = new TrailerAdapter(this, trailerList);
+
+        recyclerView = findViewById(R.id.recycler_view1);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        loadJSON();
+
+    }
+
+    private void loadJSON() {
+
+        movie_id = getIntent().getExtras().getInt("id");
+        try {
+            if (BuildConfig.THE_MOVIE_DB_API_TOKEN.isEmpty()) {
+                Toast.makeText(this, "Please obtain your API Key from themoviedb.org", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Client client = new Client();
+            Service apiService = Client.getClient().create(Service.class);
+            Call<TrailerResponse> call = apiService.getMovieTrailer(movie_id, "pt-BR", BuildConfig.THE_MOVIE_DB_API_TOKEN);
+            call.enqueue(new Callback<TrailerResponse>() {
+                @Override
+                public void onResponse(Call<TrailerResponse> call, Response<TrailerResponse> response) {
+                    trailer = response.body().getResults();
+                    recyclerView.setAdapter(new TrailerAdapter(getApplicationContext(), trailer));
+                    recyclerView.smoothScrollToPosition(0);
+                }
+
+                @Override
+                public void onFailure(Call<TrailerResponse> call, Throwable t) {
+                    Log.d("Error", t.getMessage());
+                    Toast.makeText(DetailActivity.this, "Error fetching trailer data", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        } catch (Exception e) {
+            Log.d("Error", e.getMessage());
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
